e716764a806489f774fc6880975e616fb20bc07a
MkGodh <fenryrvla@yandex.ru>
2024-03-29 16:35:06 +0300

    Добавьте файлы проекта.

---
+++ ToyShop/ToyShop.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+
+
+
+int main() {
+	using namespace std;
+	setlocale(LC_ALL, "RUS");
+
+	float x;
+	float y;
+	float z;
+
+	cout << "Введите высоту брусса: "; cin >> x;
+	cout << "Введите ширину брусса: "; cin >> y;
+	cout << "Введите длинну брусса: "; cin >> z;
+	
+	if (x < 0 || y < 0 || z < 0) {
+		std::cout << "Вы ввели неверное значение! Пожалуйста, введите положительные числа." << "\n";
+		return 0;
+	}
+
+	float cubeResult = ((int)y * (int)z * (int)x) / pow(x, 3);
+	cout << "Из этого бруска можно изготовить кубиков: " << cubeResult << "\n";
+	
+	if (cubeResult < 8) {
+		cout << "Из такого колличества кубиков нельзя составить набор!";
+	}
+	for (float counter = 1; counter < cubeResult; counter++) {
+		float result = pow(counter, 3);
+		if (result > cubeResult) {
+			counter--;
+			float result = pow(counter, 3);
+			cout << "Из них можно составить набор из " << result << "\n";
+			break;
+		}
+	}
+}
